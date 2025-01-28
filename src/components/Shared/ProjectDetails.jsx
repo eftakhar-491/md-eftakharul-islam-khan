14ce40b703aa6f54747570f653aca5e82e2c5cd7
MD EFTAKHARUL ISLAM KHAN <eftt042@gmail.com>
2025-01-28 23:36:17 +0600

    working on details page

--- src/components/Shared/ProjectDetails.jsx
+++ src/components/Shared/ProjectDetails.jsx
@@ -1,5 +1,18 @@
 import React from "react";
-
+import bg from "../../assets/bg.jpg";
 export default function ProjectDetails() {
-  return <div>ProjectDetails</div>;
+  return (
+    <>
+      <section>
+        <div
+          style={{
+            background: `linear-gradient(190deg, #32196290 , #32196290), url(${bg}) `,
+          }}
+          className="h-[300px] bg-no-repeat bg-cover bg-center"
+        >
+          sdfsad
+        </div>
+      </section>
+    </>
+  );
 }
