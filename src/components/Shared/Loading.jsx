0e45d0bcf8180bea963f82f11d5d49b61bb3833c
MD EFTAKHARUL ISLAM KHAN <eftt042@gmail.com>
2025-02-02 19:12:14 +0600

    checking a bug

--- src/components/Shared/Loading.jsx
+++ src/components/Shared/Loading.jsx
@@ -4,7 +4,11 @@ export default function Loading({ data }) {
   //   if (data.length > 0) return null;
   return (
     <>
-      <section className="slide-up fixed top-0 left-0 w-full h-full text-white bg-black bg-opacity-50 flex justify-center items-center z-[80]">
+      <section
+        className={`${
+          data.length > 0 && "slide-up"
+        } fixed top-0 left-0 w-full h-full text-white bg-black bg-opacity-50 flex justify-center items-center z-[80]`}
+      >
         <div className="tracking-[30px]">
           {data.length === 0 && (
             <>
